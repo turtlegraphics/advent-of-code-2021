a4fe9f6fdc0a31a68eac0ad2e8c6990f88a12f8e
Bryan Clair <bryan@slu.edu>
2021-12-06 01:00:47 -0600

    Found a bug in my not-equals method

--- aocutils.py
+++ aocutils.py
@@ -94,6 +94,9 @@ class Point:
     def __eq__(self,other):
         return (self.x == other.x) and (self.y == other.y)
 
+    def __ne__(self,other):
+        return not self == other
+
     def __add__(self,other):
         newpt = self.__copy__()
         newpt += other
